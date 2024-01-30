2c5c940d5e447bc7079a8cc05cfbb1d8fd1b20ab
Christian Tripodi <skl11@icloud.com>
2024-01-30 18:05:22 +0100

    lezione

--- m1/s2/g2/D2.js
+++ m1/s2/g2/D2.js
@@ -2,6 +2,8 @@
  Scrivi un algoritmo per trovare il più grande tra due numeri interi.
 */
 
+{
+
 let numero1 = 5;
 let numero2 = 7;
 
@@ -14,13 +16,39 @@ else
 {
 max = numero2;
 }
-console.log ("Massimo = " + max);
+console.log ('Massimo = ' + max)
 
+}
+
+{
+
+let x = 4;
+let y = 7;
+
+if(x>y)
+{
+  console.log ('il più grande è' + x);
+}else if(x>y){
+  console.log ('il più grande è' + y);
+}else{
+  console.log ('inumeri sono uguali');
+}
+
+}
 
 /* ESERCIZIO 2
   Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
 */
 
+{
+
+  let x = 8;
+
+  if(x !==5) console.log('Not Equal');
+
+
+
+}
 
 
 /* ESERCIZIO 3
@@ -29,13 +57,34 @@ console.log ("Massimo = " + max);
 
 
 
+{
+
+let x = 10;
+
+if(x % 5 === 0) console.log('divisibile per 5');
+
+// se non ha resto da il risultato divisibile per 5
+
+
+}
+
 
 
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
+{
 
+  let x = 4;
+  let y = 4;
+
+if(x === 8 || y === 8 || (x + y) === 8 || (x - y) === 8 || (y -x) === 8  ){
+  console.log('verificato');
+  
+}
+
+}
 
 
 
@@ -47,6 +96,23 @@ console.log ("Massimo = " + max);
 */
 
 
+{
+
+let totalShoppingCart = 80;
+let totaleDaPagare = totalShoppingCart;
+let costospedizione = 10;
+
+if(totalShoppingCart <50){
+  totaleDaPagare += costoSpedizione
+
+}
+console.log(totaleDaPagare)
+
+
+
+}
+
+
 
 
 
@@ -55,7 +121,33 @@ console.log ("Massimo = " + max);
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+{
+
+  let totalShoppingCart = 80;
+  totalShoppingCart *= 0.8;
+  let totaleDaPagare = totalShoppingCart;
+  let costospedizione = 10;
+  
+  if(totalShoppingCart <50){
+    totaleDaPagare += costoSpedizione
+  
+  }
+  console.log(totaleDaPagare)
+  
+  
+  
+  
+  
+  
+  }
+  
+
+
+
+
+
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -63,19 +155,50 @@ console.log ("Massimo = " + max);
   Alla fine mostra il risultato in console.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+//tanti if annidati
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+let valore = 'test';
+
+if(typeof valore === 'number'){
+  console.log('è un numero');
+}else{
+  console.log('non è un numero');
+}
+
+
+
+
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+{
+
+
+let x = 11;
+if(x % 2 === 0){
+  console.log('pari');
+}else{
+  console.log('dispari');
+}
+
+
+
+
+}
+
+
+
+
+
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
@@ -89,7 +212,29 @@ console.log ("Massimo = " + max);
     }
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+{
+
+  let val = 7
+  if (val < 5) {
+      console.log("Meno di 10");
+    } else if (val < 5) {
+      console.log("Meno di 5");
+    } else {
+      console.log("Uguale a 10 o maggiore");
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
@@ -101,28 +246,47 @@ const me = {
   skills: ['javascript', 'html', 'css'],
 }
 
+me.city ='Toronto';
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+delete me.lastName
+
+
+
+
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+me.skills.pop()
+
+
+
+
+
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let arr =[];
+arr.push (1,2,3,4,5,6,7,8,9,10)
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+{
+
+  
+  arr.splice(9,1,100)
+
+}
