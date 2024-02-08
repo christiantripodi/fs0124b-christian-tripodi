36746c7681e49713c323716ce24b7f1028d4ab0a
Christian Tripodi <skl11@icloud.com>
2024-02-08 09:27:10 +0100

    ultim

---
+++ m1/s3/g3/assets/js/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+const botton = document.getElementById('send')
+
+botton.addEventListener('click', ilMioTask)
+
+function ilMioTask (){
+
+    let nuovoDiv = document.createElement('div')
+
+ let valore = document.getElementById('task').value
+
+ 
+
+
+ console.log(valore)
+}
+
+
+         
