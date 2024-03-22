95085ae8d78a4f545fa16d6c2ccd6749d52f1535
christiantripodi <skl11@icloud.com>
2024-03-22 12:54:42 +0100

    comm

---
+++ m3/s1/g5/esercizio/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { HeaderComponent } from './main-components/header/header.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FiatComponent,
+    FordComponent,
+    AudiComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
