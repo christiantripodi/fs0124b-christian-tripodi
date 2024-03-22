80774cf751910a213fbdc3fe79141bb08373e274
christiantripodi <skl11@icloud.com>
2024-03-22 15:52:57 +0100

    notwork

--- m3/s1/g5/esercizio/src/app/app-routing.module.ts
+++ m3/s1/g5/esercizio/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { FordComponent } from './pages/ford/ford.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'Fiat',
+    component: FiatComponent
+  },
+  {
+    path:'Audi',
+    component: AudiComponent
+  },
+  {
+    path:'Ford',
+    component: FordComponent
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- m3/s1/g5/esercizio/src/app/app.module.ts
+++ m3/s1/g5/esercizio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { HeaderComponent } from './main-component/header/header.component';
+import { FooterComponent } from './main-component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FiatComponent,
+    AudiComponent,
+    FordComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
